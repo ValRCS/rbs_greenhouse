d4e8519a48cfe4634fc3311c1f16365b685fdadf
Valdis Saulespurens <valdis.s.coding@gmail.com>
2025-01-21 15:22:03 +0200

    added Venn Diagram example

---
+++ notebooks/test.py
@@ -0,0 +1,7 @@
+# show python version
+import sys
+print(f"Python version: {sys.version}")
+# show python path
+print(f"Python path: {sys.path}")
+# show which python executable is being used
+print(f"Python executable: {sys.executable}")
